3a509b58318ce0a908646e6505af64382c7ab7e2
minniejung <jungmignonne@gmail.com>
2025-04-21 20:02:23 +0900

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ['aronlaszka.com', 'cataas.com', 'cataas.comhttps'],
+		domains: [
+			'aronlaszka.com',
+			'cataas.com',
+			'cataas.comhttps',
+			'jade-biological-gamefowl-447.mypinata.cloud',
+			'bafybeig7ow34ri2tzrnwr2n26gxdmml72au52h7r32zp6cw7j6w4segkdu',
+			'lavender-left-coyote-594.mypinata.cloud',
+		],
 	},
 }
 
